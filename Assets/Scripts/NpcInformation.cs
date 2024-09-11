62ca1d4f85308496c552bf96ff77a75a321fdd9b
DraconicLegacy <richie.weldon@icloud.com>
2024-09-11 13:59:07 -0700

    Started on a quest database

--- Assets/Scripts/NpcInformation.cs
+++ Assets/Scripts/NpcInformation.cs
@@ -5,21 +5,31 @@ using UnityEngine;
 [CreateAssetMenu]
 public class NpcInformation : MonoBehaviour
 {
-    
     public string NpcName = "PH";
     [Space]
     public bool canBeInteractedWith = true;
     public bool isAttackableByPlayer = false;
     [Space]
     public int NpcMaxHealth = 100;
+    public int NpcHealth = 100;
     public int NpcDamage = 1;
     public int NpcArmor = 1;
-
+    [Space]
+    public int NpcQuestsGiven = 0;
+    public int NpcDialogueOptions = 0;
+    [Space]
     GameObject homePoint;
-
+    QuestDatabase quests;
     void Start ()
     {
+        quests = GetComponent<QuestDatabase>();
         homePoint = new GameObject("homePoint" + NpcName);
         homePoint.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        quests.LoadQuests();
+        Debug.Log(homePoint.transform.position);
+        Debug.Log(homePoint.name);
+        Debug.Log(this.transform.position);
+        Debug.Log(this.NpcName);
+        Debug.Log(this.NpcQuestsGiven);
     }
 }
