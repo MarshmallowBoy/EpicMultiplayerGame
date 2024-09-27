9064e1d1dd06ea919e28e62683c0b0f783539e7e
MarshmallowBoy <traysoncazier719@gmail.com>
2024-09-27 11:38:17 -0700

    Baked Lights

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -8,6 +8,13 @@ public class Health : MonoBehaviour
     public Animator animator;
     public Character character;
     public Animator anim2;
+    public Vector3 Spawningpoint;
+
+    public void Start()
+    {
+        Spawningpoint = transform.position;
+    }
+
     void Update()
     {
         animator.SetFloat("Health", health);
@@ -24,7 +31,8 @@ public class Health : MonoBehaviour
         yield return new WaitForSeconds(3);
         anim2.Play("Fade");
         yield return new WaitForSeconds(1);
-        transform.position = new Vector3(192.5f, 100f, 276.35f);
+        //transform.position = new Vector3(192.5f, 100f, 276.35f);
+        transform.position = Spawningpoint;
         character.enabled = true;
         health = 100;
         
