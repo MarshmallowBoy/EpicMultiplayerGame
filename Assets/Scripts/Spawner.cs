5ac0100939292f4671222f93b8b8e7c552544701
DraconicLegacy <richie.weldon@icloud.com>
2024-10-03 14:02:34 -0700

    Updated Spawner
    Still bugged, Will be fixing

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,23 +7,30 @@ public class Spawner : MonoBehaviour
     public GameObject[] enemiesToSpawn;
     [Space] 
     float timer;
+    float _timer;
     public float timeToSpawn = 30;
 
     void Update()
     {
         timer += Time.deltaTime;
+        _timer += Time.deltaTime;
         if(timer >= timeToSpawn)
         {
-            DoSpawn();
+            for (int i = Random.Range(0, enemiesToSpawn.Length); i < enemiesToSpawn.Length; i++)
+            {
+                if (_timer >= 3)
+                {
+                    DoSpawn();
+                }
+            }
+            timer = 0;
         }
     }
 
     void DoSpawn()
     {
-        for (int i = Random.Range(0, enemiesToSpawn.Length); i < enemiesToSpawn.Length; i++)
-        {
-                Instantiate(enemiesToSpawn[Random.Range(1, enemiesToSpawn.Length)]);
-                Debug.Log("Spawned an Enemy");
-        }
+        Instantiate(enemiesToSpawn[Random.Range(1, enemiesToSpawn.Length)]);
+        Debug.Log("Spawned an Enemy");
+        _timer = 0;
     }
 }
