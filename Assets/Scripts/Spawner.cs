0f6d8d39f761563506e0b8150b0ec31fc2815d31
DraconicLegacy <richie.weldon@icloud.com>
2024-10-03 13:39:27 -0700

    Started on the Spawner Script

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject[] enemiesToSpawn;
+    [Space] 
+    float timer;
+    public float timeToSpawn = 30;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if(timer >= timeToSpawn)
+        {
+            DoSpawn();
+        }
+    }
+
+    void DoSpawn()
+    {
+        for (int i = Random.Range(0, enemiesToSpawn.Length); i < enemiesToSpawn.Length; i++)
+        {
+                Instantiate(enemiesToSpawn[Random.Range(1, enemiesToSpawn.Length)]);
+                Debug.Log("Spawned an Enemy");
+        }
     }
 }
