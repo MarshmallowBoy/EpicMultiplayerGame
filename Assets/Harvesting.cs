25cf0de4c80f8abd7f36f891ce32bceace36d5a5
MarshmallowBoy <traysoncazier719@gmail.com>
2024-10-22 11:35:39 -0700

    push baby push

--- Assets/Harvesting.cs
+++ Assets/Harvesting.cs
@@ -8,16 +8,17 @@ public class Harvesting : MonoBehaviour
     public GameObject ParticleSystem;
     public GameObject TextThing1;
     public GameObject TextThing2;
+    public int ID;
+    public int GiveAmount;
     public void FixedUpdate()
     {
         slider.value += 0.05f;
         if(slider.value >= 100)
         {
-            InventoryManager.Instance.GiveItem(1);
-            InventoryManager.Instance.GiveItem(1);
-            InventoryManager.Instance.GiveItem(1);
-            InventoryManager.Instance.GiveItem(1);
-            InventoryManager.Instance.GiveItem(1);
+            for(int i = 0; i < GiveAmount; i++)
+            {
+                InventoryManager.Instance.GiveItem(ID);
+            }
             ParticleSystem.SetActive(false);
             TextThing1.SetActive(false);
             TextThing2.SetActive(true);
